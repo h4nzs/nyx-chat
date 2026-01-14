cd392be2554c115384648f88e8e5c8c71b15e96d
h4nzs <namakamunya2@gmail.com>
2026-01-14 15:00:23 +0700

    fix redis

--- server/src/lib/redis.ts
+++ server/src/lib/redis.ts
@@ -1,9 +1,9 @@
-import { createClient } from 'redis';
+import { createClient, type RedisClientType } from 'redis';
 
-export const redisClient = createClient({
+export const redisClient: RedisClientType = createClient({
   // Konfigurasi default akan menggunakan redis://127.0.0.1:6379
   // Tambahkan URL di sini jika Redis Anda berjalan di tempat lain
-  // url: process.env.REDIS_URL
+  url: process.env.REDIS_URL
 });
 
 redisClient.on('error', err => console.error('❌ Redis Client Error', err));
