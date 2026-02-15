d41c7c7563e88ed32d2137f395c4428989b587ec
h4nzs <namakamunya2@gmail.com>
2026-02-15 13:50:40 +0700

    fix: analytics

--- web/src/utils/analytics.ts
+++ web/src/utils/analytics.ts
@@ -1,6 +1,6 @@
 import ReactGA from "react-ga4";
 
-const GA_MEASUREMENT_ID = "G-0WB1ZJYZPX "; // Ganti punya lu
+const GA_MEASUREMENT_ID = "G-0WB1ZJYZPX"; // Ganti punya lu
 
 export const initGA = () => {
   ReactGA.initialize(GA_MEASUREMENT_ID);
