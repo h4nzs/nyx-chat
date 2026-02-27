df434d64e6c7b1a89f05bd80acb766edc913b5e4
h4nzs <namakamunya2@gmail.com>
2026-02-28 04:14:27 +0700

    fix: still upload r2

--- server/src/utils/r2.ts
+++ server/src/utils/r2.ts
@@ -8,8 +8,12 @@ export const s3Client = new S3Client({
   credentials: {
     accessKeyId: env.r2AccessKeyId,
     secretAccessKey: env.r2SecretAccessKey
-  }
-  // Nggak usah pake forcePathStyle, biar R2 pake default-nya
+  },
+  // 1. WAJIB BUAT R2: Biar URL-nya gak jadi subdomain aneh (chat-uploads.671c...)
+  forcePathStyle: true,
+  // 2. OBAT ANTI-AAAAAA==: Larang AWS SDK ngitung checksum file kosong!
+  requestChecksumCalculation: 'WHEN_REQUIRED',
+  responseChecksumValidation: 'WHEN_REQUIRED'
 })
 
 // Generate URL upload yang valid selama 5 menit
