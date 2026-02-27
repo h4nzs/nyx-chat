c0d16feed6f01146a231a7d02268b9ee57b4156e
h4nzs <namakamunya2@gmail.com>
2026-02-28 04:32:26 +0700

    fix: still upload r2

--- server/src/utils/r2.ts
+++ server/src/utils/r2.ts
@@ -9,9 +9,6 @@ export const s3Client = new S3Client({
     accessKeyId: env.r2AccessKeyId,
     secretAccessKey: env.r2SecretAccessKey
   },
-  // 1. WAJIB BUAT R2: Biar URL-nya gak jadi subdomain aneh (chat-uploads.671c...)
-  forcePathStyle: true,
-  // 2. OBAT ANTI-AAAAAA==: Larang AWS SDK ngitung checksum file kosong!
   requestChecksumCalculation: 'WHEN_REQUIRED',
   responseChecksumValidation: 'WHEN_REQUIRED'
 })
@@ -25,8 +22,7 @@ export const getPresignedUploadUrl = async (key: string, contentType: string) =>
   })
 
   const url = await getSignedUrl(s3Client, command, {
-    expiresIn: 300,
-    signableHeaders: new Set(['content-type'])
+    expiresIn: 300
   })
 
   return url
