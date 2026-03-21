3a50531c6cb60a41adacdb77c622bc71f2535603
h4nzs <namakamunya2@gmail.com>
2026-03-22 04:36:31 +0700

    fix: neon prisma

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,12 +1,16 @@
 import 'dotenv/config'
 import { PrismaClient } from '@prisma/client'
 import { PrismaNeon } from '@prisma/adapter-neon'
+import { neonConfig } from '@neondatabase/serverless'
+import ws from 'ws'
 
 const connectionString = process.env.DATABASE_URL;
 
 if (!connectionString) {
   throw new Error('DATABASE_URL is not defined in the environment variables.');
 }
+
+neonConfig.webSocketConstructor = ws;
 
 // Instantiate the Prisma adapter
 const adapter = new PrismaNeon({
