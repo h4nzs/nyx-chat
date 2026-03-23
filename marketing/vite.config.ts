d34faf25c73135f64750ea9be1a2996590318323
h4nzs <namakamunya2@gmail.com>
2026-03-24 03:31:05 +0700

    chore: new marketing instances

--- marketing/vite.config.ts
+++ marketing/vite.config.ts
@@ -1,19 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 import packageJson from './package.json';
 
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   define: {
     // Mendefinisikan variabel global agar dikenali oleh aplikasi
     __APP_VERSION__: JSON.stringify(packageJson.version),
   },
   resolve: {
     alias: {
-      // Memberitahu vite bahwa '@store' mengarah ke folder src/store
-      '@store': path.resolve(__dirname, './src/store'),
+      '@store': path.resolve(__dirname, '../web/src/store'), // 👈 Arahkan ke store utama
+      // Jika Anda mengimport komponen dengan '../components', tangkap aliasnya:
+      '@components': path.resolve(__dirname, '../web/src/components'),
+      // Atau arahkan semua import yang spesifik
     },
   },
 })
