191d8f05ebc9c0c4c9a39ead3ad2cc5116f59361
h4nzs <namakamunya2@gmail.com>
2026-03-30 08:25:31 +0700

    fix: db

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 import { PrismaClient } from '@prisma/client';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
